42d83178688cb95207b0ec6b51e19a7a63d25148
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2023-05-14 16:41:10 +0200

    #40 - modification queryBuilder pour la sélection des utilisateurs. accès seulement utilisateur avec role ROLE_ADMIN

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,10 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -18,11 +24,34 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        return parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters)
+            ->andWhere('entity.roles NOT LIKE :role')
+            ->setParameter('role', '%ROLE_ADMIN%')
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('username')
+            ->add('email')
+            ->add('createdAt')
+        ;
+    }
+
     public function configureActions(Actions $actions): Actions
     {
         return $actions
             ->remove(Crud::PAGE_DETAIL, Action::INDEX)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+
             ->setPermission(CRUD::PAGE_DETAIL, 'USER_PROFILE')
+            ->setPermission(CRUD::PAGE_INDEX, 'ROLE_ADMIN')
         ;
     }
 
@@ -37,6 +66,9 @@ class UserCrudController extends AbstractCrudController
 
     public function configureCrud(Crud $crud): Crud
     {
-        return $crud->setPageTitle(CRUD::PAGE_DETAIL, 'Profil');
+        return $crud
+            ->setPageTitle(CRUD::PAGE_DETAIL, 'Profil')
+            ->setPageTitle(CRUD::PAGE_INDEX, 'Utilisateurs')
+        ;
     }
 }
