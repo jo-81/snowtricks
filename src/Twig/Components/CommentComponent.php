2685186741371a1bb7f0f02a93076c5fc4c8d897
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2023-05-23 15:07:06 +0200

    #21 - Création formulaire ajout commentaire seulement pour utilisateur connecté. Gestion du formulaire

--- src/Twig/Components/CommentComponent.php
+++ src/Twig/Components/CommentComponent.php
@@ -2,25 +2,35 @@
 
 namespace App\Twig\Components;
 
+use App\Entity\Comment;
 use App\Entity\Trick;
+use App\Entity\User;
+use App\Form\Comment\CommentType;
 use App\Repository\CommentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('comment')]
-final class CommentComponent
+final class CommentComponent extends AbstractController
 {
     use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    public const NUMBER_COMMENT = 3;
 
     #[LiveProp]
     public ?Trick $trick = null;
 
-    public const NUMBER_COMMENT = 3;
-
     #[LiveProp(writable: true)]
     public int $page = 0;
+
+    public bool $isPersist = false;
 
     public function __construct(private CommentRepository $commentRepository)
     {
@@ -29,6 +39,32 @@ final class CommentComponent
     public function mount(Trick $trick): void
     {
         $this->trick = $trick;
+    }
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->createForm(CommentType::class, new Comment());
+    }
+
+    #[LiveAction]
+    public function addComment(): Response
+    {
+        $this->submitForm();
+        /** @var Comment $comment */
+        $comment = $this->getFormInstance()->getData();
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $comment
+            ->setTrick($this->trick)
+            ->setAuthor($user)
+        ;
+
+        $this->commentRepository->save($comment, true);
+        $this->addFlash('success', 'Votre commentaire a bien été ajouté');
+
+        return $this->redirectToRoute('trick.show', ['slug' => $this->trick->getSlug()]); /* @phpstan-ignore-line */
     }
 
     #[LiveAction]
