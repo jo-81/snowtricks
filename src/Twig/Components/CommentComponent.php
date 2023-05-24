a9226f6d72cf20a484eb7bce2329bfebe1cc597b
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2023-05-24 15:22:05 +0200

    #70 - ajout formulaire pour signaler un commentaire

--- src/Twig/Components/CommentComponent.php
+++ src/Twig/Components/CommentComponent.php
@@ -2,19 +2,21 @@
 
 namespace App\Twig\Components;
 
-use App\Entity\User;
-use App\Entity\Trick;
 use App\Entity\Comment;
+use App\Entity\CommentSignaled;
+use App\Entity\Trick;
+use App\Entity\User;
 use App\Form\Comment\CommentType;
 use App\Repository\CommentRepository;
+use App\Repository\CommentSignaledRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
-use Symfony\UX\LiveComponent\DefaultActionTrait;
-use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
-use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('comment')]
 final class CommentComponent extends AbstractController
@@ -25,11 +27,16 @@ final class CommentComponent extends AbstractController
     #[LiveProp]
     public Trick $trick;
 
+    #[LiveProp(writable: true)]
+    public ?string $reason = null;
+
     /** @var array<mixed> */
     public array $changes = [];
 
-    public function __construct(private CommentRepository $commentRepository)
-    {
+    public function __construct(
+        private CommentRepository $commentRepository,
+        private CommentSignaledRepository $commentSignaledRepository
+    ) {
     }
 
     protected function instantiateForm(): FormInterface
@@ -76,6 +83,38 @@ final class CommentComponent extends AbstractController
 
         $this->changes['label'] = 'success';
         $this->changes['message'] = 'Votre commentaire a bien été supprimé';
+    }
+
+    #[LiveAction]
+    public function signaledComment(#[LiveArg] int $id): void
+    {
+        /** @var Comment $comment */
+        $comment = $this->commentRepository->find($id);
+        if (!$comment instanceof Comment) {
+            $this->changes['label'] = 'danger';
+            $this->changes['message'] = 'Un problème est survenue';
+
+            return;
+        }
+
+        $isExists = $this->commentSignaledRepository->findOneBy(['comment' => $comment]);
+        if ($isExists instanceof CommentSignaled) {
+            $this->changes['label'] = 'danger';
+            $this->changes['message'] = 'Ce commentaire a déjà été signalé';
+
+            return;
+        }
+
+        $commentSignaled = new CommentSignaled();
+        $commentSignaled
+            ->setReason($this->reason) /* @phpstan-ignore-line */
+            ->setComment($comment)
+        ;
+
+        $this->commentSignaledRepository->save($commentSignaled, true);
+
+        $this->changes['label'] = 'success';
+        $this->changes['message'] = 'Votre signalement a bien été enregistré';
     }
 
     /**
